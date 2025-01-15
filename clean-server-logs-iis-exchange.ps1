1911a7707f72c8db633726fb21df29bcfb161449
Vladimir Mikhalev <10498744+heyvaldemar@users.noreply.github.com>
2025-01-15 11:51:01 -0500

    Automatically Clean Server Logs for IIS and Exchange Server

--- clean-server-logs-iis-exchange.ps1
+++ clean-server-logs-iis-exchange.ps1
@@ -1,32 +1,3 @@
-# Automatically Clean Server Logs for IIS and Exchange Server
-
-# Author
-# I’m Vladimir Mikhalev, the Docker Captain, but my friends can call me Valdemar.
-# https://www.docker.com/captains/vladimir-mikhalev/
-
-# My website with detailed IT guides: https://www.heyvaldemar.com/
-# Follow me on YouTube: https://www.youtube.com/channel/UCf85kQ0u1sYTTTyKVpxrlyQ?sub_confirmation=1
-# Follow me on Twitter: https://twitter.com/heyValdemar
-# Follow me on Instagram: https://www.instagram.com/heyvaldemar/
-# Follow me on Threads: https://www.threads.net/@heyvaldemar
-# Follow me on Mastodon: https://mastodon.social/@heyvaldemar
-# Follow me on Bluesky: https://bsky.app/profile/heyvaldemar.bsky.social
-# Follow me on Facebook: https://www.facebook.com/heyValdemarFB/
-# Follow me on TikTok: https://www.tiktok.com/@heyvaldemar
-# Follow me on LinkedIn: https://www.linkedin.com/in/heyvaldemar/
-# Follow me on GitHub: https://github.com/heyvaldemar
-
-# Communication
-# Chat with IT pros on Discord: https://discord.gg/AJQGCCBcqf
-# Reach me at ask@sre.gg
-
-# Give Thanks
-# Support on GitHub: https://github.com/sponsors/heyValdemar
-# Support on Patreon: https://www.patreon.com/heyValdemar
-# Support on BuyMeaCoffee: https://www.buymeacoffee.com/heyValdemar
-# Support on Ko-fi: https://ko-fi.com/heyValdemar
-# Support on PayPal: https://www.paypal.com/paypalme/heyValdemarCOM
-
 Set-Executionpolicy RemoteSigned
 
 $days=30
